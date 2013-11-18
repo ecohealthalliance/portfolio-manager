801440c0c191ecca0c2421f1b46e94738f69e8b8
Amy Slagle <slagle@ehatek.org>
2013-11-18 11:08:46 -0500

    Script to import disease definitions.

---
+++ import_diseases.py
@@ -0,0 +1,19 @@
+import pymongo
+import contextlib
+from urllib import urlopen
+
+db = pymongo.Connection('localhost', 3002)['meteor']
+
+diseases = db.diseases
+
+CKAN_URL = "https://ckan-datastore.s3.amazonaws.com/2013-11-12T18:22:32.991Z/google-define-disease-definitions.csv"
+
+with contextlib.closing(urlopen(CKAN_URL)) as raw_csv:
+	for line in raw_csv.read().split('\n')[1:]:
+		if line:
+			name, definition, synonyms = line.split(',')
+			diseases.insert({
+                'name': name,
+                'definition': definition,
+                'synonyms': [synonym for synonym in synonyms.split('  ') if synonym],
+			})
