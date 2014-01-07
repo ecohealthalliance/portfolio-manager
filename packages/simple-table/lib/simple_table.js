0ff055040252065f53a41ff6e20ac2fd0a128020
Amy Slagle <slagle@ehatek.org>
2014-01-07 12:15:39 -0500

    Add pagination to table.

--- packages/simple-table/lib/simple_table.js
+++ packages/simple-table/lib/simple_table.js
@@ -1,6 +1,15 @@
 var getSessionSortKey = function (identifier) {
-    return identifier + '-simple-table-sort-key';
-}
+    return identifier + '-simple-table-sort';
+};
+
+var getSessionRowsPerPageKey = function (identifier) {
+    return identifier + '-simple-table-rows-per-page';
+};
+
+var getSessionCurrentPageKey = function (identifier) {
+    return identifier + '-simple-table-current-page';
+};
+
 
 if (Handlebars) {
     Handlebars.registerHelper('simpleTable', function (identifier, collection, fields, attrs) {
@@ -10,6 +19,8 @@ if (Handlebars) {
             fields = _.without(_.keys(collection.findOne()), '_id');
         }
         Session.setDefault(getSessionSortKey(identifier), fields[0].key);
+        Session.setDefault(getSessionRowsPerPageKey(identifier), 10);
+        Session.setDefault(getSessionCurrentPageKey(identifier), 0);
         var html = Template.simpleTable({identifier: identifier, collection: collection, fields: fields, attrs: attrs});
         return new Handlebars.SafeString(html);
     });
@@ -41,7 +52,24 @@ Template.simpleTable.helpers({
         var sortKey = Session.get(getSessionSortKey(this.identifier));
         var sortQuery = {};
         sortQuery[sortKey] = 1;
-        return this.collection.find({}, {sort: sortQuery});
+        var limit = Session.get(getSessionRowsPerPageKey(this.identifier));
+        var currentPage = Session.get(getSessionCurrentPageKey(this.identifier));
+        var skip = currentPage * limit;
+        return this.collection.find({}, {sort: sortQuery, skip: skip, limit: limit});
+    },
+
+    "getRowsPerPage" : function () {
+        return Session.get(getSessionRowsPerPageKey(this.identifier));
+    },
+
+    "getCurrentPage" : function () {
+        return 1 + Session.get(getSessionCurrentPageKey(this.identifier));
+    },
+
+    "getPageCount" : function () {
+        var rowsPerPage = Session.get(getSessionRowsPerPageKey(this.identifier));
+        var count = this.collection.find().count();
+        return Math.ceil(count / rowsPerPage);
     }
 });
 
@@ -50,5 +78,25 @@ Template.simpleTable.events({
         var sortKey = $(event.target).attr("key");
         var identifier = $(event.target).parents('.simple-table').attr('simple-table-id');
         Session.set(getSessionSortKey(identifier), sortKey);
+    },
+
+    "change .simple-table-navigation .rows-per-page": function (event) {
+        try {
+            var rowsPerPage = parseInt($(event.target).val(), 10);
+            var identifier = $(event.target).parents('.simple-table-navigation').attr('simple-table-id');
+            Session.set(getSessionRowsPerPageKey(identifier), rowsPerPage);
+        } catch (e) {
+            console.log("rows per page must be an integer");
+        }
+    },
+
+    "change .simple-table-navigation .current-page": function (event) {
+        try {
+            var currentPage = parseInt($(event.target).val(), 10) - 1;
+            var identifier = $(event.target).parents('.simple-table-navigation').attr('simple-table-id');
+            Session.set(getSessionCurrentPageKey(identifier), currentPage);
+        } catch (e) {
+            console.log("rows per page must be an integer");
+        }
     }
 });
