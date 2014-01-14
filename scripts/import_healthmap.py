cad47c1c1de1033f8d7406f385a14b358f02a7af
Amy Slagle <slagle@ehatek.org>
2014-01-14 17:58:24 -0500

    Remove print statement.

--- scripts/import_healthmap.py
+++ scripts/import_healthmap.py
@@ -79,7 +79,6 @@ if __name__ == '__main__':
                                 'date': date,
                                 'source': 'healthmap',
                             })
-                            print "Imported %s" % title
                         imported_resources.append(resourceId)
 
     portfolios.insert({
